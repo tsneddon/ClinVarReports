ba85050881537d4c87f08648284bf429021906bc
tsneddon <tsneddon@users.noreply.github.com>
2019-05-21 15:16:30 -0400

    Updated to include generation of Distribution file

--- ClinVarExcelReports.py
+++ ClinVarExcelReports.py
@@ -3,9 +3,9 @@ from ExcelReportsFunctions import *
 def main():
 
     inputFile1 = 'ClinVarVariationRelease_00-latest.xml.gz'
-    inputFile2 = 'submission_summary.txt.gz'
-    inputFile3 = 'variation_allele.txt.gz'
-    inputFile4 = 'variant_summary.txt.gz'
+    inputFile2 = 'variation_allele.txt.gz'
+    inputFile3 = 'variant_summary.txt.gz'
+    inputFile4 = 'submission_summary.txt.gz'
 
     arg = sys.argv[1]
     #Options are 'ZeroStar', 'OneStar'
@@ -13,22 +13,23 @@ def main():
     dir = 'ClinVar' + arg + 'Reports'
 
     get_file(inputFile1, '/pub/clinvar/xml/clinvar_variation/')
-    date = get_file(inputFile2, '/pub/clinvar/tab_delimited/')
+    get_file(inputFile2, '/pub/clinvar/tab_delimited/')
     get_file(inputFile3, '/pub/clinvar/tab_delimited/')
-    get_file(inputFile4, '/pub/clinvar/tab_delimited/')
+    date = get_file(inputFile4, '/pub/clinvar/tab_delimited/')
+
+    create_orgDict(inputFile1)
+    create_a2vHash(inputFile2)
+    create_HGVSHash(inputFile3)
+    create_scvHash(inputFile4, arg)
 
     ExcelDir = make_directory(dir, date, arg)
 
     excelFile = arg + 'Report_' + date + '.xlsx'
+    distFile = '_' + arg + 'DistributionReport_' + date + '.xlsx'
 
     statFile = '_' + arg + 'ReportsStats_' + date + '.xlsx'
 
-    create_orgDict(inputFile1)
-    create_scvHash(inputFile2, arg)
-    create_a2vHash(inputFile3)
-    create_HGVSHash(inputFile4)
-
     create_files(ExcelDir, excelFile, date, statFile, arg)
+    create_distFile(ExcelDir, distFile, date, arg)
 
 main()
-
